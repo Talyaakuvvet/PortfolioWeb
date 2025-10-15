b497947fdfb7300385298ce69b0038ed2aad355f
talya kuvvet <talyakuvvet04@gmail.com>
2025-10-15 15:06:03 +0300

    about kısmı değişikliği

--- react-portfolio/react-portfolio/src/components/hero/hero.jsx
+++ react-portfolio/react-portfolio/src/components/hero/hero.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styles from "./hero.module.css";
 
 import heroImage from '../../../assets/hero/heroImage.png';
@@ -9,9 +8,7 @@ export const Hero = () => {
        <div className={styles.content}>
          <h1 className={styles.title}>Hi, I'm Talya</h1>
          <p className={styles.description}>
-         I am a third-year Software Engineering student specializing in iOS development.
-Recently, I developed a strong interest in artificial intelligence and have been expanding
-my knowledge in this area. Reach out if you'd like to learn more!
+         I am a senior Software Engineering student at Atlas University. I have past experience in Swift projects and basic web technologies. Recently, my main focus has shifted to artificial intelligence.
          </p>
          <a href="mailto:talyakuvvet04@gmail.com" className={styles.contactBtn}>
            Contact Me
